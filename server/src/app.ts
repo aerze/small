eb5849a81de4389d25577818b44f07185d896cee
Jeremy Jung <jeremyjung@users.noreply.github.com>
2017-01-09 22:28:53 -0800

    refactor server code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,19 +1,50 @@
 import * as express from 'express'
 import * as logger from 'morgan'
-
+import * as http from 'http'
 import * as path from 'path'
+import * as sio from 'socket.io'
 
 export default class App {
-  public app: express.Application
+  private app: express.Application
+  private io: SocketIO.Server
+
+  public connectedClients: number = 0
+  public server: http.Server
 
   constructor() {
     this.app = express()
+    this.server = http.createServer(this.app)
+    this.io = sio(this.server)
+
+    this.sockets(this.io)
     this.middleware(this.app)
     this.routes(this.app)
   }
 
+  public start(port: number): void {
+    this.server.listen(port)
+    this.server.on('listening', () => {
+      const address = this.server.address()
+      console.log(`listening on port ${address.port}`)
+    })
+  }
+
+  public stop(): void {
+    this.server.close()
+  }
+
   private middleware(app: express.Application): void {
     app.use(logger('dev'))
+  }
+
+  private sockets(io: SocketIO.Server): void {
+    io.on('connection', (socket) => {
+
+      console.log(`Clients connected: ${++this.connectedClients}`)
+      socket.on('disconnect', () => {
+        console.log(`Clients connected: ${--this.connectedClients}`)
+      })
+    })
   }
 
   private routes(app: express.Application) {
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,8 @@
-import * as http from 'http'
-import * as sio from 'socket.io'
-
 import App from './app'
 
-const app = new App().app
+const app = new App()
 const port = 8080
+app.start(port)
 
-const server = http.createServer(app)
 
-//Setup Socket.IO
-//Trying things here before extracting out to another class
-const io = sio(server)
-
-let numberOfClients: number = 0
-io.on('connection', (socket) => {
-
-  console.log(`Clients connected: ${++numberOfClients}`)
-  socket.on('disconnect', () => {
-    console.log(`Clients connected: ${--numberOfClients}`)
-  })
-})
-
-server.listen(port)
-server.on('listening', () => {
-  const address = server.address()
-  console.log(`listening on port ${address.port}`)
-})
 
